614187cc9dca7ed5eef270e316636614c14f08d5
larsonn <lars@feldeisen.de>
2022-03-06 01:01:56 +0100

    setConnector

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import * as _ from 'lodash'
 
 const store: any = {};
 const listeners: any = {};
+const connector: { setter: any, getter: any} = {setter: () => {}, getter: () => {}}
 
 function _isSet(key: string) {
     return listeners[key] && listeners[key]?.has
@@ -13,6 +14,8 @@ function _unsubscribe(key: string, callback: () => {}) {
 
 export function dispatch(key: string, value: any) {
     store[key] = _.cloneDeep(value);
+    console.log("STORE",store);
+    connector.setter(store);
     if(!listeners[key]) return;
     listeners[key].forEach(
         (listener: (arg0: any) => any) => listener(store[key])
@@ -30,4 +33,15 @@ export function getStore(key: string | undefined) {
     if(key === undefined) return _.cloneDeep(store);
     return _.cloneDeep(store[key]);
 
+}
+
+export function setConnector(setter: () => {}, getter: () => {}) {
+    connector.setter = setter;
+    connector.getter = getter;
+    const fromGetter = getter();
+    if(!fromGetter) return;
+    
+    Object.keys(fromGetter).forEach(key => {
+        store[key] = fromGetter[key];
+    })
 }
